7d915af6e3e83983fa222dc38d6eebedf69fc782
Oskar <o.skoglund09@gmail.com>
2021-01-18 17:12:54 +0100

    Light switch with stick

---
+++ Optimization Assignment/Assets/Scripts/HallwayButton.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine.Events;
+using UnityEngine;
+
+public class HallwayButton : MonoBehaviour
+{
+    private string buttonTrigger;
+    public UnityEvent toggle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        buttonTrigger = "buttonTrigger";
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(buttonTrigger))
+        {
+            toggle?.Invoke();
+        }
+    }
+}
